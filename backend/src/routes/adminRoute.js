bb025674a81d3d19ba15b29b450bc29efb86a1be
Gabriel Palmieri <gabriel1208.palmieri@gmail.com>
2025-08-22 16:01:05 -0300

    ssda

--- backend/src/routes/adminRoute.js
+++ backend/src/routes/adminRoute.js
@@ -99,6 +99,6 @@ router.post('/ticket/:ticketId/rate', authenticated, authorizeRole(['Admin']), r
 router.post('/agent/:agentId/evaluate', authenticated, authorizeRole(['Admin']), createAgentEvaluationController);
 router.get('/agent/:agentId/evaluations', authenticated, authorizeRole(['Admin']), getAgentEvaluationsController);
 router.get('/agent/:agentId/evaluation-stats', authenticated, authorizeRole(['Admin']), getAgentEvaluationStatsController);
-router.get('/agents/evaluations', authenticated, authorizeRole(['Admin', 'Client']), getAllAgentsWithEvaluationsController);
+router.get('/agents/evaluations', authenticated, authorizeRole(['Admin', 'Client', 'Agent']), getAllAgentsWithEvaluationsController);
 
 export default router;
